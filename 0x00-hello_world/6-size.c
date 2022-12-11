2cb207650e096a70e28082f330ad235083e73029
khalif114 <khalifm2022@gmail.com>
2022-12-11 00:00:12 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - print out sizes of data types in C
- * code by khalifa114
- * Return: 0
-*/
+ * main -entry point
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
-	printf("size of an int:%zu byte(s)\n", sizeof(int));
-	printf("size of a long int:%zu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of a float:%zu byte(s)\n", sizeof(float));
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
