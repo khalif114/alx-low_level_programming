707bf323315e2ee31a20835fbf6caa07d841f2d4
khalif114 <khalifm2022@gmail.com>
2022-12-16 13:29:11 -0800

    task 1 fouth

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,5 @@
-nclude "main.h"
+#nclude "main.h"
+
 /**
  * _isdigit - function that checks for a digit (0 through 9)
  * @c: input
@@ -6,9 +7,5 @@ nclude "main.h"
  */
 int _isdigit(int c)
 {
-
-if ((c >= 48) && (c <= 57))
-return (1);
-else
-return (0);
+return (c > = '0' && c <= '9');
 }
